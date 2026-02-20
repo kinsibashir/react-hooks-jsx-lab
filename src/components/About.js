b958ca421a594fc5ba70ab0ceac45d3765df45ab
kinsibashir <kinsibashir@gmail.com>
2026-02-20 04:49:26 -0800

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; 
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love building modern web apps with React and creating clean, reusable code.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
